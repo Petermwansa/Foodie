194d9904db7eda16bfe47ee9f7dba52f107bc1c8
Petermwansa <mwansapeter800@gmail.com>
2025-05-05 20:45:29 +0300

    Admin side complete

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,19 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
+import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useColorScheme } from 'react-native';
 import CartProvider from '../provider/CartProvider';
-
-
-import { useColorScheme } from '@/src/components/useColorScheme';
-
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
-
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -56,12 +54,11 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <CartProvider>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+          <Stack.Screen name="(user)" options={{ headerShown: false }} />
           <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
         </Stack>
       </CartProvider>
     </ThemeProvider>
   );
 }
-
-
