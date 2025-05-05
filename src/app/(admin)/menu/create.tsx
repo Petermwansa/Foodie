3270545115a8aa2e1b9b012bf18e3739154f0ca8
Petermwansa <mwansapeter800@gmail.com>
2025-05-05 21:56:06 +0300

    Add the create a product form

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,33 +1,88 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native'
 import React, { useState } from 'react'
 import Button from '@components/Button'
+import { defaultPizzaImage } from '@components/ProductListItem';
+import Colors from '@/src/constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
+import { Stack } from 'expo-router';
+
 
 const CreateProductScreen = () => {
 
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
+    const [errors, setErrors] = useState('');
+    const [image, setImage] = useState<string | null>(null);
+
 
     const resetFields = () => {
         setName('');
         setPrice('');
+    };
+
+    // here we do input validation 
+    const validateInput = () => {
+        setErrors('')
+        if (!name) {
+            setErrors("Name is required");
+            return false
+        }
+        if (!price) {
+            setErrors("Price is required");
+            return false
+        }
+        if (isNaN(parseFloat(price))) {
+            setErrors("Price is not a number");
+            return false
+        }
+
+        return true;
     }
+
+
+    // the image picker fnction 
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
     const onCreate = () => {
         console.warn("Creating a product");
 
+        // here we check if the validating the data is true then we can go ahead and submit the data
+        if (!validateInput()) {
+            return
+        }
 
+        // save in the database
         resetFields();
     }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Name</Text>
-      <TextInput value={name} onChangeText={setName} placeholder='Name' style={styles.input}/>
+        <Stack.Screen options={{ title: "Create a product"}}/>
+        <Image style={styles.image} source={{ uri: image || defaultPizzaImage }} />
+        <Text onPress={pickImage} style={styles.textButton} >Select an Image</Text>
+        <Text style={styles.label}>Name</Text>
+        <TextInput value={name} onChangeText={setName} placeholder='Name' style={styles.input}/>
 
-      <Text style={styles.label}>Price</Text>
-      <TextInput value={price} onChangeText={setPrice} placeholder='Price' style={styles.input} keyboardType='numeric'/>
+        <Text style={styles.label}>Price</Text>
+        <TextInput value={price} onChangeText={setPrice} placeholder='Price' style={styles.input} keyboardType='numeric'/>
 
-      <Button text='Create' onPress={onCreate}/>
+        <Text style={{ color: 'red'}}>{errors}</Text>
+        <Button text='Create' onPress={onCreate}/>
     </View>
   )
 }
@@ -45,6 +100,17 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginTop: 5,
         marginBottom: 20
+    },
+    image: {
+        width: '50%',
+        aspectRatio: 1,
+        alignSelf: 'center'
+    },
+    textButton: {
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: Colors.light.tint,
+        marginVertical: 10,
     },
     label: {
         color: 'grey',
